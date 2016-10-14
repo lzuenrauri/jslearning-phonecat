df50c801a600681a8f34bb9fcc6c47e6191962af
Happyplankton <lzuenrauri@users.noreply.github.com>
2016-10-14 14:58:14 +0800

    Fixed the problem...forgot the post-tag </selection>

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,15 @@
-function PhoneListCtrl($scope) {
+var myApp = angular.module('myApp',[]);
+myApp.controller('PhoneListCtrl', ['$scope', function ($scope) {
 	$scope.welcome = "Hello, our customers!";
-  	$scope.phones = [
+  $scope.phones = [
     {"name": "Nexus S",
-     "snippet": "Fast just got faster with Nexus S."},
+     "snippet": "Fast just got faster with Nexus S.",
+ 	 "age": 0},
     {"name": "Motorola XOOM™ with Wi-Fi",
-     "snippet": "The Next, Next Generation tablet."},
+     "snippet": "The Next, Next Generation tablet.",
+ 	 "age": 1},
     {"name": "MOTOROLA XOOM™",
-     "snippet": "The Next, Next Generation tablet."}
+     "snippet": "The Next, Next Generation tablet.",
+ 	 "age": 2}
   ];
-}
+}]);
