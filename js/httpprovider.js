d8a10cb7ecad5c5a59af17a767bf62df89b5764b
Happyplankton <lzuenrauri@users.noreply.github.com>
2016-10-14 17:12:09 +0800

    use  to load json file

---
+++ js/httpprovider.js
@@ -0,0 +1,8 @@
+var myApp = angular.module('myApp',[]);
+myApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get('./phones/phones.json').success(function(data) {
+    $scope.phones = data;
+  });
+
+  $scope.orderProp = 'age';
+}]);
